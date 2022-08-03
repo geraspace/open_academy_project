b3a6e7af12f81c0866c088b6fd2ca2f4c1e6e2fc
geraspace <gerardo@vauxoo.com>
2022-08-03 14:20:21 -0700

    [IMP] open_academy: Add sessions and instructor field in contact T#59013
    - Add new page "sessions" in contact
    - Add "instructor" boolean field inside "sessions" page
    - Add session records inside "sessions" page

--- open_academy/__manifest__.py
+++ open_academy/__manifest__.py
@@ -14,6 +14,7 @@
         "security/ir.model.access.csv",
         "views/open_academy_course_views.xml",
         "views/open_academy_session_views.xml",
+        "views/res_partner_views.xml",
         "views/open_academy_menus_views.xml"
         ],
     "demo": [
