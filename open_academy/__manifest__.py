aeefef45b83df1b65356986ac0d48406349bf7ca
geraspace <gerardo@vauxoo.com>
2022-08-13 09:02:50 -0700

    [IMP] open_academy: Session report and dashboard T#59013
    - Add report that contains start and end date and attendees of a session.
    - Add dashboard that contains graph view, sessions calendar a list view of the courses.

--- open_academy/__manifest__.py
+++ open_academy/__manifest__.py
@@ -8,7 +8,7 @@
     "license": "LGPL-3",
     "version": "15.0.1.0.1",
     "depends": [
-        "base"
+        "board"
         ],
     "data": [
         "security/security.xml",
@@ -16,8 +16,10 @@
         "views/open_academy_course_views.xml",
         "views/open_academy_session_views.xml",
         "views/res_partner_views.xml",
+        "views/open_academy_menus_views.xml",
+        "views/open_academy_session_board.xml",
         "wizards/open_academy_wizard_views.xml",
-        "views/open_academy_menus_views.xml"
+        "reports/open_academy_session_report.xml"
         ],
     "demo": [
         "demo/open_academy_course_demo.xml"
